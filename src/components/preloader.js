8e9d6d7d5aa71165af8d54bc062448e2f6533f12
Sanyam Sharma <84903641+Sanyam17-beep@users.noreply.github.com>
2023-08-12 18:33:55 +0530

    Update preloader.js
    Clean code 
    less calls

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -7,39 +7,31 @@ function Preloader() {
     let temp=Math.floor(Math.random()*193);
     const [i,seti]=useState(temp); 
     const loading = () => {
-      document.querySelectorAll(".bar").forEach(function(current) {
         let startWidth = 0;
         const interval = setInterval(frame,100);
-        const inte = setInterval(fr,4000);
+        const inte = setInterval(()=>{
+            let temp=Math.floor(Math.random()*193);
+            seti(temp);
+        },4000);
         function frame() {
           if (startWidth >= 100) {
             clearInterval(inte);
             clearInterval(interval);
           } else {
             startWidth++;
-            current.style.width = `${startWidth}%`;
             setsize(startWidth);
           }
         }
-        function fr(){
-          let temp=Math.floor(Math.random()*193);
-          seti(temp);
-        } 
-      });
-    }
-    function fro(){
-        let temp=Math.floor(Math.random()*193);
-        seti(temp);
-      } 
+        }
     useEffect(() => {
       setTimeout(loading, 1000);
     }, []);
   return (
     <div className='pre'>
-      <div className="loader" onClick={fro}>
+      <div className="loader" onClick={() => seti(Math.floor(Math.random() * 193))}>
       <div className="load"><span id='name'>Sanyam</span>
       <div className="progress">
-        <div className="bar">
+        <div className="bar" style={{ width: `${size}%` }}>
         </div>
       </div>
       <span id="percent">{size}%</span>
